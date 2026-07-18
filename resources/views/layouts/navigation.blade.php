fd4288ecc87602330579f37cc58c8112219165b6
Sazzad <sazzad14802@gmail.com>
2026-07-19 04:03:00 +0600

    Replace laravel logo with QuickOnline

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <h1 class="text-xl font-bold text-gray-800">QuickOnline</h1>
                 </div>
 
                 <!-- Navigation Links -->
